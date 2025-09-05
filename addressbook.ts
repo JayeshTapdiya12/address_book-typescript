a66c30f9c3d72369ba676693216490c0d6b6647a
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-05 12:25:34 +0530

    [jayesh tapdiya] added the contact class in the address book to add new contact , uc1 done

--- addressbook.ts
+++ addressbook.ts
@@ -1,1 +1,34 @@
 //  this is the main branch of the address book
+
+class Contact {
+  fname: string;
+  lname: string;
+  address: string;
+  city: string;
+  state: string;
+  zipcode: number;
+  phoneNumber: number;
+  email: string;
+  constructor(
+    fname: string,
+    lname: string,
+    address: string,
+    city: string,
+    state: string,
+    zipcode: number,
+    phoneNumber: number,
+    email: string
+  ) {
+    this.fname = fname;
+    this.lname = lname;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+  toString(): string {
+    return `${this.fname} ${this.lname} , ${this.address}, ${this.city}, ${this.state}, ${this.zipcode}, ${this.phoneNumber}, ${this.email}`;
+  }
+}
